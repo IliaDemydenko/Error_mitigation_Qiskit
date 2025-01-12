e73fdf207b121073cdbc1d24a79129aed1773d3f
IliaDemydenko <163906740+IliaDemydenko@users.noreply.github.com>
2025-01-12 15:50:54 +0200

    More detailed comments

--- Noise_mitigation.py
+++ Noise_mitigation.py
@@ -1,3 +1,5 @@
+#This code impelements the simplest method of obtaining the measurement result without noises on quantum computer, which is called ZNE - Zero Noise Extrapolation
+
 from qiskit import *
 import numpy as np
 from qiskit_aer import AerSimulator
@@ -5,7 +7,7 @@ from qiskit.visualization import plot_histogram
 from qiskit_aer.noise import NoiseModel, depolarizing_error
 import matplotlib.pyplot as plt
 
-#creation of simple quantum circuit
+#This circuit prepares three states on Bloch sphere with angles phi and theta. Then two CX gates entangle 0-th and 1-st and 1-st and 2-nd qubits
 phi = np.array([0, np.pi/4, np.pi/2])
 theta = np.array([0, np.pi/4, np.pi/2])
 
@@ -22,7 +24,7 @@ qc.measure(0,0)
 qc.measure(1,1)
 qc.measure(2,2)
 
-#measurements without depolarizing error
+#Here we measure our ciircuit with AerSimulator without error (ideal quantum computer). Further we focus on the probability of obtaining state |100>
 shots_number = 100000
 backend = AerSimulator(method='statevector', shots=shots_number)
 result = backend.run(qc).result()
@@ -31,7 +33,7 @@ shots_result = result.get_counts()
 p_without_error = shots_result["100"] / shots_number
 print(p_without_error)
 
-#measurements with error with several error rates
+#Here we add depolarizing errors for all single-qubit gates with different error rates. The result is the set of probabilities of obtaining state |100> for different error rates
 error_rates = np.array([0.01, 0.02, 0.03, 0.04, 0.05])
 shots_number = 100000
 p = []
@@ -49,7 +51,7 @@ for error_rate in error_rates:
 
 print(p)
 
-#interpolation of the dependence of result on error rate
+#Here we extrapolate dependence of the probability of obtaining |100> on error rate values. We use linear extrapolation
 degree = 1
 poly_coefficients = np.polyfit(error_rates, p, degree)
 poly_model = np.poly1d(poly_coefficients)
@@ -62,7 +64,7 @@ plt.plot(x, y)
 
 plt.show()
 
-#obtaining of approximated result of observable without error and its comparison to exact result
+#Here we use ZNE (Zero Noise Extrapolation) to obtain the result of the measurement without noise and then compare it with exact result
 p_approx = poly_model(0)
 print(p_approx)
 
